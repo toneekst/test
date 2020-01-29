cd57978ac0e7fded6b7f4b4713291f4a6a17dabb
toneekst <mr.tonee@mail.ru>
2020-01-29 20:41:57 +0300

    one123

--- smg.py
+++ smg.py
@@ -1,17 +1,4 @@
 print ("hello")
 
 
-print ("commit")
-
-
-"""justanothercommit"""
-print
-
-"""justanothercommit"""
-print ("commit")
-
-"""justanothercommit"""
-print ("commit")
-
-"""justanothercommit"""
-print ("commit") ("commit")
+"""shitness"""
